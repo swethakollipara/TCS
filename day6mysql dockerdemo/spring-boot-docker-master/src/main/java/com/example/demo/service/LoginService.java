a21b7360aee0c5dd501b10c0ef9f0f2d740623ea
swethakollipara <saiswetha.k22@gmail.com>
2022-05-24 12:12:39 +0000

    day6mysqldockerdemo

---
+++ day6mysql dockerdemo/spring-boot-docker-master/src/main/java/com/example/demo/service/LoginService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Login;
+import com.example.demo.exceptions.UserNotFoundException;
+import com.example.demo.repository.LoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    LoginRepository loginRepository;
+
+        // this ur  your service
+    public Login checkUsername(String username) {
+
+        Optional<Login> loginDetails = loginRepository.findByUsername(username);
+        
+     
+        
+        
+        if (!loginDetails.isPresent()) throw new UserNotFoundException(username);
+
+        return loginDetails.get();
+    }
+}
