5e3076608ffb08ae4ad3191b5e5d7a2c6353a475
swethakollipara <saiswetha.k22@gmail.com>
2022-06-03 11:06:27 +0000

    day13 springsecurityjwt modified

---
+++ day13sp-scuritymodifier/spring-security-jwt-modified/src/main/java/com/demo/springsecurity/controller/HelloWorldController.java
@@ -0,0 +1,60 @@
+package com.demo.springsecurity.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.springsecurityjwt.models.AuthenticationRequest;
+import com.demo.springsecurityjwt.models.AuthenticationResponse;
+import com.demo.springsecurityjwt.service.MyUserDetailsService;
+import com.demo.springsecurityjwt.util.JwtUtil;
+
+@RestController
+public class HelloWorldController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
+
+	@GetMapping({ "/hellosecondPage" })
+	public String secondPage() {
+		return "how are you";
+	}
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+
+}
