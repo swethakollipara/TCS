974d296fa1c465e9879866f0f5109fff1f4a067a
swethakollipara <saiswetha.k22@gmail.com>
2022-06-06 11:05:39 +0000

    modified project details

--- Project/PensionerDetailMicroservice/src/main/java/com/demo/PensionerDetailMicroservice.java
+++ Project/PensionerDetailMicroservice/src/main/java/com/demo/PensionerDetailMicroservice.java
@@ -1,11 +1,11 @@
 package com.demo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+
 public class PensionerDetailMicroservice {
 
 	public static void main(String[] args) {
